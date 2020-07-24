7a6390b2d19b5dfd451ae3c58d6a25701cb05618
LiGuo <liguo1976@outlook.com>
2020-07-24 11:33:46 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,7 @@ fi
 #拉取镜像
 echo "pull image: $DRONE_IMAGENAME"
 registry=$(echo "$DRONE_IMAGENAME" | awk -F'/' '{print $1}')
-if [ [ "$registry" =~ "." ] ]
+if [[ "$registry" =~ "." ]]
 then
     base64Registry=$(echo "{\"serveraddress\":\"$registry\"}" | base64)
     curl --location --request POST ''${DRONE_SERVERURL}'/api/endpoints/'$DRONE_ENDPOINTID'/docker/images/create?fromImage='$DRONE_IMAGENAME'' \

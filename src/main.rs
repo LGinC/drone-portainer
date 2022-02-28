f3dc66c8c34d41fb0bda558df28c306e35077f9b
李果 <liguo1976@outlook.com>
2022-02-28 11:49:20 +0800

    merge workflow

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ fn main() {
     println!("Hello, world!");
 
     match env::var("PLUGIN_SERVERURL") {
-        Ok(v)=> println!("PLUGIN_SERVERURL:{}", v),
+        Ok(v) => println!("PLUGIN_SERVERURL:{}", v),
         Err(e) => println!("get env err: {}", e),
     }
-    
 }
